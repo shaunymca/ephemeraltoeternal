99a8089a3cd4b952fc3d307d83a735b40ed39d4a
Matthew Bilyeu <mbilyeu@rjmetrics.com>
2017-03-16 15:54:26 -0400

    iterating on styling

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,24 +2,49 @@
   <div class="hello">
     <div>
       <input v-model="searchTerm"
-             v-on:keyup.13="search"/>
+             v-on:keyup.13="search"
+             placeholder="Search your archive"/>
     </div>
     <button v-on:click="search">Search</button>
-    <ul>
-      <li v-for="m in messages">
-        <strong>{{ m.user_name }}</strong>: {{ m.text }}
-      </li>
-    </ul>
+    <div class="results" v-if="messages.length > 0">
+      Results for '{{ resultsForTerm }}'...
+      <ul>
+        <li class="message"
+            v-for="m in messages">
+          <div class="details">
+            <span class="timestamp">
+              {{ m.timestamp | dateFormat }}
+            </span>
+            <span class="channel">
+              <a v-bind:href="m.channel_link">#{{ m.channel_name }}</a>
+            </span>
+            <span class="user">
+              <a v-bind:href="m.user_link">@{{ m.user_name }}</a>
+            </span>
+          </div>
+          <span class="text">{{ m.text }}</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'hello',
   data () {
     return {
       searchTerm: '',
+      resultsForTerm: '',
       messages: []
+    }
+  },
+  filters: {
+    dateFormat: function (date) {
+      var tsFloat = parseFloat(date) * 1000
+      return moment(tsFloat).format('MM/DD/YY h:mm A');
     }
   },
   methods: {
@@ -31,8 +56,12 @@ export default {
       }
 
       this.$http.get('/messages', opts).then(response => {
-
-        this.messages = response.body;
+        this.resultsForTerm = this.searchTerm
+        this.messages = response.body.map(m => {
+          m.channel_link = 'slack://channel?id=' + m.channel_id + '&team=' + m.team_id
+          m.user_link = 'slack://user?team=' + m.team_id + '&id=' + m.user_id
+          return m
+        })
 
       }, response => {
         alert('Something went wrong :(')
@@ -48,13 +77,30 @@ h1, h2 {
   font-weight: normal;
 }
 
+.results {
+  margin: 40px auto;
+  width: 75%;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
 }
 
-li {
+.message {
+  text-align: left;
   margin: 0 10px;
+  border-bottom: 1px solid #ececec;
+  padding: 10px 5px;
+}
+
+.text {
+  white-space: pre-line;
+}
+
+.details {
+  margin-bottom: 5px;
+  font-size: 90%;
 }
 
 a {
