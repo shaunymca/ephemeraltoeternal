1c964df7386a03edda1e7aba0cbdc64ee3b50120
Matthew Bilyeu <mbilyeu@rjmetrics.com>
2017-03-17 11:40:31 -0400

    link to home

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
   <div id="app">
       <a href="https://github.com/shaunymca/ephemeraltoeternal" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
-    <h1>ephemeral &#8594; eternal</h1>
+    <h1><a href="/">ephemeral &#8594; eternal</a></h1>
     <h2>Search your entire archive, even if you have a free account.</h2>
     <router-view></router-view>
   </div>
@@ -29,6 +29,11 @@ body {
 
 h1 {
   margin: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
 }
 
 h2 {
